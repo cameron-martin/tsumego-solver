eb6199c2f6ef0295e0f998830ffe00cefc7da438
Cameron Martin <cameronmartin123@gmail.com>
2020-04-05 15:57:56 +0100

    Improve CLI help messages

--- src/bin/cli/main.rs
+++ src/bin/cli/main.rs
@@ -8,28 +8,32 @@ use std::path::Path;
 fn main() -> io::Result<()> {
     let matches = App::new("Tsumego Solver")
         .subcommand(
-            SubCommand::with_name("explore").arg(
-                Arg::with_name("file")
-                    .short("f")
-                    .long("file")
-                    .required(true)
-                    .takes_value(true),
-            ),
+            SubCommand::with_name("explore")
+                .about("Explore the game tree of a single puzzle")
+                .arg(
+                    Arg::with_name("file")
+                        .help("The SGF file to load")
+                        .short("f")
+                        .long("file")
+                        .required(true)
+                        .takes_value(true),
+                ),
         )
         .subcommand(
             SubCommand::with_name("generate")
+                .about("Generate puzzles and output them as SGF files")
                 .arg(
                     Arg::with_name("out")
+                        .help("The directory to write the generated puzzles to")
                         .short("o")
                         .long("out")
-                        .required(true)
                         .default_value("generated_puzzles")
                         .takes_value(true),
                 )
                 .arg(
                     Arg::with_name("threads")
+                        .help("The number of threads to use")
                         .long("threads")
-                        .required(true)
                         .default_value("8")
                         .takes_value(true),
                 ),
