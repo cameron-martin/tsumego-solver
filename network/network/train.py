e0619f38dce9337102274d6ffab8f8a34ee159ec
Cameron Martin <cameron.martin@forgerock.com>
2020-06-18 12:56:14 +0100

    Add FC layer to end of convolutional layers.

--- network/network/train.py
+++ network/network/train.py
@@ -13,20 +13,20 @@ tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_fr
 with open(os.path.join(os.path.dirname(__file__), '..', '..', 'generated_puzzles', '_examples.bin'), 'rb') as f:
     contents = f.read()
 
+
 images, labels = decode(contents)
 
 model = keras.Sequential([
     keras.Input(shape=(8, 16, 3)),
-    keras.layers.Conv2D(kernel_size=3, filters=8, strides=1, padding='same', activation='relu'),
-    keras.layers.MaxPool2D(pool_size=(1, 2)),
     keras.layers.Conv2D(kernel_size=3, filters=16, strides=1, padding='same', activation='relu'),
     keras.layers.MaxPool2D(pool_size=(2, 2)),
     keras.layers.Conv2D(kernel_size=3, filters=32, strides=1, padding='same', activation='relu'),
     keras.layers.MaxPool2D(pool_size=(2, 2)),
     keras.layers.Conv2D(kernel_size=3, filters=64, strides=1, padding='same', activation='relu'),
     keras.layers.MaxPool2D(pool_size=(2, 2)),
-    keras.layers.Conv2D(kernel_size=3, filters=129, strides=1, padding='same', activation='relu'),
     keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.Dense(129),
 ])
 
 model.summary()
